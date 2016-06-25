4afbda3ee95717789230cae0c02db70f0572476d
lau-ren <colls.lauren@gmail.com>
2016-06-26 10:29:32 +1200

    add preinstall script

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import Router from './router'
 
 const router = new Router()
 console.log("in indexjs");
-router.listen(process.env.PORT || 8008, function(){
-  console.log('listening on', router.address().port);
-});
+router.listen()
