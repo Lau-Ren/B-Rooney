9ce23b7119c6c1ba8f87528c13324c287ba08a13
lau-ren <colls.lauren@gmail.com>
2016-06-27 13:24:32 +1200

    stuff

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-import server from './server.js'
-server()
+var path = require('path')
+var express = require('express')
+
+module.exports = {
+
+  var app = express()
+
+  app.use(express.static(path.join(__dirname, '../public')))
+  app.use(require('cookie-parser')())
+  app.use(require('body-parser').urlencoded({ extended: true }))
+
+  app.get('/', function (req, res) {
+
+    res.sendFile('./public/index.html')
+  })
+
+  app.listen(process.env.PORT || 3000, function () {
+    console.log('server is running on 3000!')
+  })
+}
